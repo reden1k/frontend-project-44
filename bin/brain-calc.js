3237fbc2756f3f3630b3530e22e9e578a5745aa2
reden1k <dim23062006@gmail.com>
2024-03-09 17:46:05 +0300

    finished

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,7 +4,7 @@ import { runner } from '../src/cli.js';
 
 const description = 'What is the result of the expression?';
 
-const expression = (sign, v, v1) =>  {
+const expression = (sign, v, v1) => {
   let answer;
   switch (sign) {
     case '-':
@@ -16,25 +16,28 @@ const expression = (sign, v, v1) =>  {
     case '*':
       answer = v * v1;
       break;
+    default:
+      break;
   }
   return [`Question: ${v} ${sign} ${v1}`, answer];
-}
+};
 
 function calc() {
   const array = ['+', '*', '-'];
   const value1 = Math.round(Math.random() * 100);
   const value2 = Math.round(Math.random() * 10);
-  const index = Math.round(Math.random() * 3);
-  let exp = expression(array[index], value1, value2);
-  let answer = exp[1];
-  let input;
-  console.log(exp[0])
-    input = readlineSync.question('Your answer: ');
-    if (answer === Number(input)) {
-      console.log('Correct!');
-      return true;
-    }
-    return [input, answer];
+  const index = Math.round(Math.random() * (3 - 1) + 1);
+
+  const exp = expression(array[index], value1, value2);
+  const answer = exp[1];
+
+  console.log(exp[0]);
+  const input = readlineSync.question('Your answer: ');
+  if (answer === Number(input)) {
+    console.log('Correct!');
+    return true;
   }
+  return [input, answer];
+}
 
 runner(calc, description);
