0700d718b21eb24f34643fd933f6f1e275971264
reden1k <dim23062006@gmail.com>
2024-03-09 11:14:19 +0300

    cursed bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { welcome, congratulation } from '../src/callbacks.js';
-import { name } from '../src/cli.js';
 
-export default function isEven() {
-  welcome();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let count = 0;
-  for (let i = 0; i < 3; i += 1) {
-    const number = Math.round(Math.random() * 100);
-    console.log(`${'Question:'} ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (number % 2 === 0 && answer === 'yes') {
-      console.log('Correct!');
-      count += 1;
-    } else if (number % 2 !== 0 && answer === 'no') {
-      console.log('Correct');
-      count += 1;
-    } else if (answer === 'yes' && number % 2 !== 0) {
-      console.log(`${'\'yes\' is wrong answer ;(. Correct answer was \'no\'. \nLet\'s try again,'} ${name}!`);
-      break;
-    } else if (answer === 'no' && number % 2 === 0) {
-      console.log(`${'\'no\' is wrong answer ;(. Correct answer was \'yes\'. \nLet\'s try again,'} ${name}!`);
-      break;
+import readlineSync from 'readline-sync';
+import {name} from "../src/cli.js";
+import start from "../src/cli.js";
+
+export function IsEven() {
+    start();
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    let input;
+    let answer = '';
+    for (let i = 0; i < 3; i += 1) {
+        const num = Math.round(Math.random() * 100);
+        if (num % 2 === 0) {
+            answer = 'yes';
+        } else {
+            answer = 'no';
+        }
+        console.log(`${'Question:'} ${num}`);
+        input = readlineSync.question('Your answer: ');
+        if (answer === input) {
+            console.log('Correct!');
+        }
+         else {
+        console.log(`'${input}' is wrong answer ;(. Correct answer was '${answer}' \n Let's try again ${name}!`);
+        return;
+        }
     }
-  }
-  if (count === 3) {
-    congratulation();
-  }
+    console.log(`${'Congratulations,'} ${name}!`)
 }
-isEven();
+
+IsEven();
