3667ad530c758ab1b488b1e30e9a81212dab6cbd
reden1k <dim23062006@gmail.com>
2024-03-09 11:11:56 +0300

    added bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/no-unresolved
-import * as game from '../src/cli.js';
-// eslint-disable-next-line import/no-unresolved
-console.log('Welcome to the Brain Games!');
-game.getName();
+import start from "../src/cli.js"
+start();
+
+
