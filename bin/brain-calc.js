102a922f30faa88cafa626a554a39fd1b62790da
reden1k <dim23062006@gmail.com>
2024-03-09 17:39:04 +0300

    fixed brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,44 +4,37 @@ import { runner } from '../src/cli.js';
 
 const description = 'What is the result of the expression?';
 
+const expression = (sign, v, v1) =>  {
+  let answer;
+  switch (sign) {
+    case '-':
+      answer = v - v1;
+      break;
+    case '+':
+      answer = v + v1;
+      break;
+    case '*':
+      answer = v * v1;
+      break;
+  }
+  return [`Question: ${v} ${sign} ${v1}`, answer];
+}
+
 function calc() {
   const array = ['+', '*', '-'];
   const value1 = Math.round(Math.random() * 100);
   const value2 = Math.round(Math.random() * 10);
   const index = Math.round(Math.random() * 3);
-  let answer = 0;
+  let exp = expression(array[index], value1, value2);
+  let answer = exp[1];
   let input;
-  
-  if (array[index] === '-') {
-    console.log(`Question: ${value1} - ${value2}`);
-    answer = value1 - value2;
+  console.log(exp[0])
     input = readlineSync.question('Your answer: ');
     if (answer === Number(input)) {
       console.log('Correct!');
       return true;
     }
+    return [input, answer];
   }
-
-  else if (array[index] === '*') {
-    console.log(`Question: ${value1} * ${value2}`);
-    answer = value1 * value2;
-    input = readlineSync.question('Your answer: ');
-    if (answer === Number(input)) {
-      console.log('Correct!');
-      return true;
-    }
-  }
-
-  else if (array[index] === '+') {
-    console.log(`Question: ${value1} + ${value2}`);
-    answer = value1 + value2;
-    input = readlineSync.question('Your answer: ');
-    if (answer === Number(input)) {
-      console.log('Correct!');
-      return true;
-    }
-  }
-  return [input, answer];
-}
 
 runner(calc, description);
