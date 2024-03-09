d69331755f6618bcdcea5d99462afb3a619b3476
reden1k <dim23062006@gmail.com>
2024-03-09 11:30:43 +0300

    upd brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -12,7 +12,7 @@ function calc() {
         const index = Math.round(Math.random() * 3);
         let answer = 0;
         if (array[index] === '-') {
-            console.log(`${value1} - ${value2}`)
+            console.log(`Question: ${value1} - ${value2}`)
             answer = value1 - value2;
             const input = readlineSync.question('Your answer: ')
             if (answer === Number(input)) {
