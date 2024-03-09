637bcae8422534b720bfce9cd4ff1eefd6ed81bc
reden1k <dim23062006@gmail.com>
2024-03-09 11:47:21 +0300

    added congrats function into src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 export let name = '';
 
-export default function start() {
+export function start() {
     console.log('Welcome to the Brain Games!');
     name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`)
 }
+
+export const congratulations =() => console.log(`${'Congratulations,'} ${name}!`);
