459ece4691bd1109ff8a9273cc844ff36afae1b4
reden1k <dim23062006@gmail.com>
2024-03-09 13:06:31 +0300

    fixed math random

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -28,13 +28,12 @@ function generateNums() {
     let number = Math.round(Math.random() * (40 - 1) + 1)
     const step = Math.round(Math.random() * (10 - 2) + 2)
     const randomSize = Math.round(Math.random() * (10 - 5) + 5)
-    const missedPlace = Math.round(Math.random() * (randomSize - 1) + 1)
-
+    const missedPlace = Math.round(Math.random() * ((randomSize - 1) - 1) + 1)
     for (let i = 0; i < randomSize; i += 1) {
-        arr[i] = number + step;
-        number += step;
+        arr[i] = number += step;
     }
     answer = arr[missedPlace];
+    console.log(arr, answer, missedPlace)
     
     arr[missedPlace] = '..'
     return arr.join(' ');
